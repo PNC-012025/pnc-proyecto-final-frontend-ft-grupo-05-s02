136bb89d18cf77d6ea7cc62a3e0aa0b6f762b648
JosseJuventino <jcastillo@inventa.com.sv>
2025-06-12 07:43:17 -0600

    feat: add login

--- capas-front/app/layout.tsx
+++ capas-front/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SessionWrapper from "./components/SessionWrapper";
+import { Toaster } from "@pheralb/toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <SessionWrapper>
           {children}
+          <Toaster theme="light" />
         </SessionWrapper>
       </body>
     </html>
